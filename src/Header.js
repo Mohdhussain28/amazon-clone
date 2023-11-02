dd5c08049c6dc19fd2ee58b50bc9176d14b7d055
Mohdhussain28 <mohdhussain28@outlook.com>
2023-11-02 23:28:39 +0530

    icons

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import './header.css'
-import SearchIcon from "@material-ui/icons/Search";
+import Search from '@mui/icons-material/Search';
 
 function Header() {
     return (
@@ -12,7 +12,7 @@ function Header() {
             </Link>
             <div className='header__search'>
                 <input className='header__searchInput' type="text" />
-                <SearchIcon />
+                <Search />
             </div>
 
 
